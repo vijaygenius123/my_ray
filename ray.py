1915c37d16779e2468be4a7c78762a6e7f922f55
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-11 18:06:37 +0530

    Simple lexer

--- ray.py
+++ ray.py
@@ -2,10 +2,36 @@ import sys
 
 def open_file(filename):
     data = open(filename,"r").read()
-    print(data)
+    return data
+
+def lex(file_contents):
+    token = ""
+    string = ""
+    state = 0
+    file_contents = list(file_contents)
+    for c in file_contents:
+        token += c
+        if token == " ":
+            token = ""
+        elif token == "PRINT":
+            print("PRINT Command")
+            token = ""
+        elif token == "\"":
+            if state == 0:
+                state = 1
+            elif state == 1:
+                print("String - ", string)
+                string = ""
+                state = 0
+        elif state == 1:
+            string += c
+            tok = ""
+    
+    print(token)
 
 def run():
     data = open_file(sys.argv[1])
+    lex(data)
 
 if __name__ == "__main__":
     run()
